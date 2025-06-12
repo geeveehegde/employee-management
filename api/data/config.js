9806f2f380b6251cdc1b456de0b84603fbda0dc9
aharaSetu <sachindevelopment24@gmail.com>
2025-06-12 18:00:40 +0530

    Revert "Api's Code Push"
    This reverts commit efceedcc8962ba0dd5ad3987bc7c6ccb87f8a043.

--- api/data/config.js
+++
@@ -1,8 +0,0 @@
-
-//To generate token secret is required
-var secretVariable={
-    'secret': 's3cr3t',
-}
-
-exports.secretVariable=secretVariable;
-
--- api/utils/validators/index.js
+++
@@ -1,4 +0,0 @@
-module.exports.userValidator = require('./user');
-module.exports.shiftSheduleValidator = require('./shiftShedule');
-module.exports.shiftManagementValidator = require('./shiftManagement');
-module.exports.staffValidator = require('./staff');
